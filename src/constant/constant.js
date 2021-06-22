e3be09569ffa52002e02abd50376c94cfaa70f9f
Manish Jadli <mnish.jhadli@irasys.biz>
2021-06-22 21:16:01 +0530

    centralised css

---
+++ src/constant/constant.js
@@ -0,0 +1,26 @@
+
+const Constant={
+    
+    primaryColor:{
+        color:'#f59000'
+    },
+    secondaryColor:{
+        color:'#000000'
+    },
+    defaultColor:{
+        color:'#ffffff'
+    },
+    primaryBackgrond:{
+        backgroundColor:'#f59000'
+    },
+    secondaryBackground:{
+        backgroundColor:'#000000'
+    },
+    defaultBackground:{
+        backgroundColor:'#ffffff'
+    }
+
+}
+
+
+export default Constant;
